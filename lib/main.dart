9d6bcc67ee75a530aef9865d96e27eda21602250
Daniel Adel <168005559+DanielAdel-2107@users.noreply.github.com>
2024-07-13 14:31:19 +0300

    create setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
