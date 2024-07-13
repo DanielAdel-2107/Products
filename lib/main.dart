b4919d4b0fe0cf8d93ad3e068dfae5807b6abbb2
Daniel Adel <168005559+DanielAdel-2107@users.noreply.github.com>
2024-07-13 14:25:39 +0300

    add package

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
+
